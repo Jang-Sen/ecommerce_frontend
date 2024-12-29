f630f58e1278b4840ec272bb2ed136014a6e3fc2
jangsen <dh7895210@naver.com>
2024-12-29 16:58:03 +0900

    Add product list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    try {
+      const result = await axios.get(
+        'https://localhost/api/v1/product/all?sort=createdAt&order=ASC&page=1&take=10',
+      );
+
+      setProducts(result.data.body.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={'mt-3'}>
+      <Row>
+        {products?.map((product) => (
+          <Col className={'mt-3'}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img
+                variant="top"
+                src={product.productImg}
+                style={{ height: '250px' }}
+              />
+              <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>{product.description.slice(0, 10)} ...</Card.Text>
+                <Card.Text>${product.price}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
